98165951808352d8c97b26a6d4c2765a9d0d7bb0
newusertesr26 <80463614+newusertesr26@users.noreply.github.com>
2022-08-09 18:41:32 -0400

    Add project files.

---
+++ APIAuthentication/Authentication/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIAuthentication.Authentication
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ APIAuthentication/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using APIAuthentication.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _companyContext;
+        public EmployeeController(ApplicationDbContext databaseContext)
+        {
+            _companyContext = databaseContext;
+        }
+        [HttpGet]
+        [Route("GetAllEmployee")]
+        public IEnumerable<Employee> Get()
+        {
+            return _companyContext.Employees.ToList();
+        }
+        [HttpGet("GetByID{id}")]
+        public Employee Get(int id)
+        {
+            return _companyContext.Employees.FirstOrDefault(s => s.Id == id);
+        }
+        [HttpPost]
+        [Route("AddNewEmployee")]
+        public void Post([FromBody] Employee value)
+        {
+            if(value != null)
+            {
+                _companyContext.Employees.Add(value);
+            }
+            _companyContext.SaveChanges();
+        }
+        [HttpPut("UpdateEmployee/{id}")]
+        public void Put(int id, [FromBody] Employee value)
+        {
+            var employee = _companyContext.Employees.FirstOrDefault(s => s.Id == id);
+            if (employee != null)
+            {
+                employee.Name = value.Name;
+                employee.Age = value.Age;
+                employee.CompanyName = value.CompanyName;
+                employee.City = value.City;
+                employee.Address = value.Address;
+                employee.Salary = value.Salary;
+                employee.Email = value.Email;
+                _companyContext.Entry(employee).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _companyContext.SaveChanges();
+            }
+        }
+        [HttpDelete("DeleteEmployee{id}")]
+        public void Delete(int id)
+        {
+            var student = _companyContext.Employees.FirstOrDefault(s => s.Id == id);
+            if (student != null)
+            {
+                _companyContext.Employees.Remove(student);
+                _companyContext.SaveChanges();
+            }
+        }
+    }
+}
